2586266344d76a33a7a31bc293aa4fe8e11701a1
CityFire <13787697167@163.com>
2016-09-25 22:43:53 +0800

    Initial Commit

---
+++ AnimatorDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatorDemo
+//
+//  Created by wjc on 16/9/25.
+//  Copyright © 2016年 wjc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
